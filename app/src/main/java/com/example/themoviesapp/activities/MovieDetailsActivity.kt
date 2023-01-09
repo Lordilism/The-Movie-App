6b4119bc18bf9dfd310e700fde36bca7a8f90cb9
Lordilism <shadowingkoko@gmail.com>
2023-01-10 01:02:39 +0630

    final UI Implementation

--- app/src/main/java/com/example/themoviesapp/activities/MovieDetailsActivity.kt
+++ app/src/main/java/com/example/themoviesapp/activities/MovieDetailsActivity.kt
@@ -2,19 +2,44 @@ package com.example.themoviesapp.activities
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.themoviesapp.R
+import com.example.themoviesapp.viewpods.ActorListViewPod
+import kotlinx.android.synthetic.main.activity_movie_details.*
 
 class MovieDetailsActivity : AppCompatActivity() {
-    companion object{
-        fun newIntent(contetxt:Context):Intent{
-            return Intent(contetxt,MovieDetailsActivity::class.java)
+    companion object {
+        fun newIntent(contetxt: Context): Intent {
+            return Intent(contetxt, MovieDetailsActivity::class.java)
 
         }
     }
+
+    //View Pod
+    lateinit var mVpActors: ActorListViewPod
+    lateinit var mVpCreators: ActorListViewPod
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_details)
+        setUpViewPod()
+        setUpListener()
+    }
+
+    private fun setUpListener() {
+        btnBack.setOnClickListener {
+            super.onBackPressed()
+        }
+    }
+
+    private fun setUpViewPod() {
+        mVpActors = vpActors as ActorListViewPod
+        mVpActors.setUpActorViewPod(R.color.colorPrimary,getString(R.string.lbl_actors),"")
+
+        mVpCreators = vpCreaters as ActorListViewPod
+        mVpCreators.setUpActorViewPod(R.color.colorPrimary,getString(R.string.lbl_creators),getString(R.string.lbl_more_actors))
+
+
     }
 }
--- app/src/main/java/com/example/themoviesapp/viewpods/ActorListViewPod.kt
+++ app/src/main/java/com/example/themoviesapp/viewpods/ActorListViewPod.kt
@@ -1,8 +1,10 @@
 package com.example.themoviesapp.viewpods
 
 import android.content.Context
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.widget.RelativeLayout
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.themoviesapp.adapters.BestActorAdapter
 import kotlinx.android.synthetic.main.view_pod_actor_list.view.*
@@ -15,6 +17,13 @@ class ActorListViewPod @JvmOverloads constructor(
     override fun onFinishInflate() {
         setUpActorRecyclerView()
         super.onFinishInflate()
+    }
+    fun setUpActorViewPod(backgroundColorRef: Int,titleText:String, moreTitleText:String){
+        setBackgroundColor(ContextCompat.getColor(context,backgroundColorRef))
+        tvBestActors.text= titleText
+        tvMoreActors.text= moreTitleText
+        tvMoreActors.paintFlags= Paint.UNDERLINE_TEXT_FLAG
+
     }
 
     private fun setUpActorRecyclerView() {
